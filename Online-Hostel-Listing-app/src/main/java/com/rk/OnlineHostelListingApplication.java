321e4b703201ae57bcbfc7218df268231a33f7f8
AbhishekMishra <abhishekmishra968517@gmail.com>
2025-11-05 09:00:55 +0530

    main class updated

--- Online-Hostel-Listing-app/src/main/java/com/rk/OnlineHostelListingApplication.java
+++ Online-Hostel-Listing-app/src/main/java/com/rk/OnlineHostelListingApplication.java
@@ -64,7 +64,7 @@ public class OnlineHostelListingApplication implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("hello");
 		
 	}
 	
