d90c62e6c8561ef8353c89328b7eb55ba2ee853b
AbhishekMishra <abhishekmishra968517@gmail.com>
2025-11-05 10:47:07 +0530

    changed

--- Online-Hostel-Listing-app/start.sh
+++ Online-Hostel-Listing-app/start.sh
@@ -5,11 +5,14 @@ set -e
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
+# Go inside the app folder
+cd Online-Hostel-Listing-app
+
 # Give permission to mvnw
 chmod +x mvnw
 
 # Build project
 ./mvnw clean package -DskipTests
 
-# Run jar
+# Run the JAR file
 java -jar target/*.jar
